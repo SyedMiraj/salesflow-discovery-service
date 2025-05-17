0ff4ed662008091e3b18ac31e9c41220df933540
Shahriar Miraj <smiraj2507@gmail.com>
2025-05-17 07:36:14 +0600

    first commit

---
+++ src/main/java/com/dsol/salesflow/discovery/SalesflowDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dsol.salesflow.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesflowDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesflowDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsol/salesflow/discovery/SalesflowDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsol.salesflow.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesflowDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
